d7c8e445bbc915b61420308c1d3f983c60cd62e7
rafiul hasan <rafiulhasan.bubt@gmail.com>
2022-08-29 10:15:36 +0600

    Initial Commit

---
+++ PassingDataBetweenVCS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassingDataBetweenVCS
+//
+//  Created by rafiul hasan on 29/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
